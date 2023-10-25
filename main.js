23720a6a0cb59a0c4d3fc0a76bbdf74433db3bb7
Bogdan Sergeychev <cheat.boss1@gmail.com>
2023-10-25 16:47:32 +0400

    minor optimizations and fixes

--- main.js
+++ main.js
@@ -6,47 +6,137 @@ const Lighting = new NativeClass('Terraria', 'Lighting');
 const SpriteEffects = new NativeClass('Microsoft.Xna.Framework.Graphics', 'SpriteEffects');
 const Vector2 = new NativeClass('Microsoft.Xna.Framework', 'Vector2');
 const Utils = new NativeClass('Terraria', 'Utils');
+const Item = new NativeClass('Terraria', 'Item');
+const Player = new NativeClass('Terraria', 'Player');
 const Color = new NativeClass('Microsoft.Xna.Framework.Graphics', 'Color');
+const Rectangle = new NativeClass('Microsoft.Xna.Framework', 'Rectangle');
+const DrawAnimationVertical = new NativeClass('Terraria.DataStructures', 'DrawAnimationVertical');
+const SpriteEffects_FlipHorizontally = SpriteEffects.FlipHorizontally.value__;
+const SpriteEffects_FlipVertically = SpriteEffects.FlipVertically.value__;
 
+const DrawData_Constructor = DrawData['void .ctor(Texture2D texture, Vector2 position, Rectangle sourceRect, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effect, int inactiveLayerDepth)'];
+const DrawData_Draw = DrawData['void Draw(SpriteBatch sb)'];
+const TextureAssetsItem = TextureAssets.Item;
+const ColorWhite = Color.White;
+const MainItemAnimations = Main.itemAnimations;
+const DrawAnimationVerticalFrame = DrawAnimationVertical["Rectangle GetFrame(Texture2D texture, int frameCounterOverride)"];
+const Lighting_GetColor = Lighting["Color GetColor(int x, int y)"];
+const Item_GetAlpha = Item.GetAlpha;
+const Player_GetImmuneAlpha = Player.GetImmuneAlpha;
+const Color_mul = Color["Color op_Multiply(Color a, float amount)"];
+function rectangle(x, y, width, height) {
+    const rectangle = Rectangle.new();
+    rectangle.X = x;
+    rectangle.Y = y;
+    rectangle.Width = width;
+    rectangle.Height = height;
+
+    return rectangle;
+}
+
+function vector_div(v, a) {
+	return {
+		X: v.X / a,
+		Y: v.Y / a,
+	}
+}  
+
+
+function modDraw_BackWeapon(data, p, length) {
+    data.rotation = ((Math.PI * 1.1 + Math.PI * (length * -0.001)) * p.direction) * p.gravDir; //rotate just over 180 clockwise
+    data.position.X -= 8 * p.direction; //back
+    data.position.Y -= (length * 0.2 - 16) * p.gravDir; //up
+
+    const playerBodyFrameNum = p.bodyFrame.Y / p.bodyFrame.Height;
+    if (playerBodyFrameNum == 7 || playerBodyFrameNum == 8 || playerBodyFrameNum == 9 ||
+        playerBodyFrameNum == 14 || playerBodyFrameNum == 15 || playerBodyFrameNum == 16) {
+        data.position.Y -= 2 * p.gravDir; //up
+    }
+	
+	return data;
+}
+
+let weaponFrame = 0;
 LegacyPlayerRenderer.DrawPlayer.hook((original, self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale, positionalOffsets) => {
-    const result = original(self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale, positionalOffsets);
-    const heldItem = drawPlayer.inventory[drawPlayer.selectedItem];
+    const heldItem = drawPlayer.HeldItem;
 
-    if (Main.gameMenu || heldItem === null || heldItem.type === 0 || heldItem.holdStyle !== 0 ||
-        !drawPlayer.active || drawPlayer.dead || drawPlayer.stoned ||
-        drawPlayer.itemAnimation > 0) {
-        return result;
+    if (heldItem === null || heldItem.type === 0 || heldItem.holdStyle !== 0 || !drawPlayer.active || drawPlayer.dead || drawPlayer.stoned || drawPlayer.itemAnimation > 0) {
+        original(self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale, positionalOffsets);
+        return;
     }
 
-    const weaponTexture = TextureAssets.Item[heldItem.type].Value;
-    const sourceRect = /*Main.itemAnimations[heldItem.type] === null ?*/ Utils['Rectangle Frame(Texture2D tex, int horizontalFrames, int verticalFrames, int frameX, int frameY, int sizeOffsetX, int sizeOffsetY)'](weaponTexture, 1, 1, 0, 0, 0, 0)/* : Main.itemAnimations[heldItem.type].GetFrame(weaponTexture, -1)*/;
 
-    let gWidth = sourceRect.Width;
-    let gHeight = sourceRect.Height;
     let drawX = drawPlayer.MountedCenter.X - Main.screenPosition.X;
-    let drawY = (drawPlayer.MountedCenter.Y - Main.screenPosition.Y + drawPlayer.gfxOffY) - 3;
-    const scaleItem = heldItem.scale;
-    let spriteEffects = SpriteEffects.None;
+    let drawY = (drawPlayer.MountedCenter.Y - Main.screenPosition.Y + drawPlayer.gfxOffY) - 3.0;
 
-    if (drawPlayer.direction < 0) { 
-        spriteEffects.value__ = SpriteEffects.FlipHorizontally.value__;
+    let tempSpriteEffectsValue = 0;
+    if (drawPlayer.direction < 0) {
+        tempSpriteEffectsValue = SpriteEffects_FlipHorizontally;
     }
 
     if (drawPlayer.gravDir < 0) {
-        drawY += 6;
-        spriteEffects.value__ = SpriteEffects.FlipVertically.value__ | spriteEffects.value__;
+        drawY += 6.0;
+        tempSpriteEffectsValue = SpriteEffects_FlipVertically | tempSpriteEffectsValue;
     }
 
+    let drawWeapon = DrawData.new();
 
-    const drawWeapon = DrawData.new();
+    const weaponTexture = TextureAssetsItem[heldItem.type].Value;
+	const gWidth = weaponTexture.Width;
+    let gHeight = weaponTexture.Height;
+	
+	
+	
     const weaponPosition = Vector2.new();
     weaponPosition.X = drawX;
     weaponPosition.Y = drawY;
+	
+	let sourceRect = null;
+	const itemAnimation = MainItemAnimations[heldItem.type];
+	if (itemAnimation != null) {
+		const frameCount = itemAnimation.FrameCount;
+		const frameCounter = itemAnimation.TicksPerFrame;
+		
+		if (Main.time % frameCounter == 0) {
+			weaponFrame++
+			if (weaponFrame >= frameCount) {
+				weaponFrame = 0;
+			}
+		}
+		
+		gHeight /= frameCount;
+		
+		sourceRect = rectangle(0, gHeight * weaponFrame, gWidth, gHeight);
+	} else {
+		sourceRect = rectangle(0, 0, gWidth, gHeight);
+	}
+	
+	const playerCenterTile = vector_div(drawPlayer.Center, 16);
+	let lighting = Lighting_GetColor(playerCenterTile.X, playerCenterTile.Y);
+	lighting = Player_GetImmuneAlpha(drawPlayer, Color_mul(Item_GetAlpha(heldItem, lighting), drawPlayer.stealth), 0);
+	
     const origin = Vector2.new();
-    origin.X = gWidth / 2;
-    origin.Y = gHeight / 2;
-    drawWeapon['void .ctor(Texture2D texture, Vector2 position, Rectangle sourceRect, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effect, int inactiveLayerDepth)'](weaponTexture, weaponPosition, sourceRect, Color.White, 0, origin, scaleItem, spriteEffects, 0);
-    drawWeapon['void Draw(SpriteBatch sb)'](Main.spriteBatch);
+    origin.X = gWidth / 2.0;
+    origin.Y = gHeight / 2.0;
+
+    let spriteEffects = SpriteEffects.None;
+    spriteEffects.value__ = tempSpriteEffectsValue;
+	const heldItemScale = heldItem.scale;
+	const itemWidth = gWidth * heldItemScale;
+    const itemHeight = gHeight * heldItemScale;
+	const larger = Math.max(itemWidth, itemHeight);
+    const lesser = Math.min(itemWidth, itemHeight);
+	
+	let data = {
+		rotation: 0.0,
+		position: weaponPosition
+	}
+	
+	data = modDraw_BackWeapon(data, drawPlayer, larger);
+
+    DrawData_Constructor(drawWeapon, weaponTexture, data.position, sourceRect, lighting, data.rotation, origin, heldItemScale, spriteEffects, 0);
+	
+    DrawData_Draw(drawWeapon, Main.spriteBatch);
 
     original(self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale, positionalOffsets);
 });
